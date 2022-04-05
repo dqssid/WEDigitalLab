c27b72748c33777deba37f08d9d7490a1b474463
fdliak <afrilfadli940@gmail.com>
2022-04-05 09:03:29 +0700

    adminn

---
+++ application/views/admin/v_sidebar.php
@@ -0,0 +1,38 @@
+<body>
+    <div class="wrapper">
+        <div class="sidebar" data-image="<?php echo base_url();?>template/admin/assets/img/sidebar-5.jpg">
+            <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <a href="javascript:;" class="simple-text">
+                      Your Logo
+                    </a>
+                </div>
+                <ul class="nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="dashboard.html">
+                            <i class="nc-icon nc-icon nc-paper-2"></i>
+                            <p>First example</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./user.html">
+                            <i class="nc-icon nc-bell-55"></i>
+                            <p>Second example</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item active active-pro">
+                        <a class="nav-link active" href="javascript:;">
+                            <i class="nc-icon nc-alien-33"></i>
+                            <p>Upgrade plan</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-panel">
