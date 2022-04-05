8559a8683eff76e9f68290858432a5ef6f0a5b09
Kurniarmdhni <ramadanikurnia511@gmail.com>
2022-04-05 10:23:52 +0700

    master

--- application/views/admin/v_navbar.php
+++ application/views/admin/v_navbar.php
@@ -1,4 +1,7 @@
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+
+<main class="main-content position-relative border-radius-lg ">
+    <!-- Navbar -->
+	<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
