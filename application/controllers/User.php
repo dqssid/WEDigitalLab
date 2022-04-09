14e56da81c7e51effd2e263b46fd3b076cad4322
Kurniarmdhni <ramadanikurnia511@gmail.com>
2022-04-09 10:53:30 +0700

    master

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class User extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,10 +20,11 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
+		
 		$this->load->view('admin/v_header');
 		$this->load->view('admin/v_sidebar');
 		$this->load->view('admin/v_navbar');
-		$this->load->view('admin/v_home');
+		$this->load->view('admin/v_user');
 		$this->load->view('admin/v_footer');
 	}
 }
